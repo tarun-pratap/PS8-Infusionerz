267ac00cae7f8adb6945b6b22f32d8fd6c70f30d
Devrastogii280 <devrastogii280@gmail.com>
2023-07-21 18:14:30 +0530

    made-ind_policy_page-updated-routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
+import Hero from "./components/Hero";
+import Ind_Policy_Page from "./components/Ind_Policy_Page";
 import Login from "./components/Login";
 import PolicyForm from "./components/PolicyForm";
 import PolicyPage from "./components/PolicyPage";
 import Sign from "./components/Sign";
 import { Route, Routes } from "react-router-dom";
+import Overview from "./components/Policy_Components/Overview";
+import KeyAdvantage from "./components/Policy_Components/KeyAdvantage";
+import HowThisWorks from "./components/Policy_Components/HowThisWorks";
+import Eligibility from "./components/Policy_Components/Eligibility";
+import Downloads from "./components/Policy_Components/Downloads";
 
 function App() {
   return (
@@ -12,6 +19,13 @@ function App() {
         <Route path="/login-user" element={<Login />} />
         <Route path="/policy-redirect-page" element={<PolicyPage />} />
         <Route path="/policy-form" element={<PolicyForm />} />
+        <Route path="/" element={<Hero />} />        
+        <Route path="/ind-policy-page/:id" element={<Ind_Policy_Page />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/key-advantages" element={<KeyAdvantage />} />
+        <Route path="/how-this-works" element={<HowThisWorks />} />
+        <Route path="/eligibility" element={<Eligibility />} />
+        <Route path="/downloads" element={<Downloads />} />
       </Routes>
     </>
   );
