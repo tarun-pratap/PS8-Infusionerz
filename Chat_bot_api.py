3764a419ac95abe483ec5a026be29d0e0f72c183
Hardik Dharmani <ddharmani99@gmail.com>
2023-07-21 16:14:18 +0530

    Created a function ask_question that takes 2 parameters one policy name and other the question to be asked to AI Bot.

--- Chat_bot_api.py
+++ Chat_bot_api.py
@@ -22,11 +22,11 @@ chatllm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 # Define the directories for policy embeddings
 policy_embeddings_directories = {
-    "Goal Assure": "./pickled policy embeddings/Goal Assure",
-    "ACE": "./pickled policy embeddings/ACE",
-    "Diabetic Care": "./pickled policy embeddings/Diabetic Care",
-    "E Touch": "./pickled policy embeddings/e-touch",
-    "Assured Wealth": "./pickled policy embeddings/Assured Wealth"
+    "Goal Assure": "./pickled_policy_embeddings/Goal Assure",
+    "ACE": "./pickled_policy_embeddings/ACE",
+    "Diabetic Care": "./pickled_policy_embeddings/Diabetic Care",
+    "E Touch": "./pickled_policy_embeddings/e-touch",
+    "Assured Wealth": "./pickled_policy_embeddings/Assured Wealth"
 }
 
 # Initialize the policy retrievers
@@ -38,19 +38,23 @@ for policy_name, embeddings_directory in policy_embeddings_directories.items():
     )
     policy_retriever = policy_db.as_retriever(search_type="similarity", search_kwargs={"k": 3})
     policy_retrievers[policy_name] = policy_retriever
-    
+
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
+
 # Initialize the conversational AI bots for each policy
 policy_ai_bots = {}
-chat_memories={}
 for policy_name, policy_retriever in policy_retrievers.items():
-    memory = ConversationBufferMemory(
-        memory_key=policy_name,
-        return_messages=True
-    )
-    chat_memories[policy_name]=memory
     policy_ai_bot = ConversationalRetrievalChain.from_llm(
         chatllm,
         retriever=policy_retriever,
-        memory=chat_memories[policy_name]
+        memory=memory,
     )
     policy_ai_bots[policy_name] = policy_ai_bot
+
+def ask_question(policy_ai_bot, question):
+    result = policy_ai_bot({"question": question})
+    answer = result["answer"]
+    return answer
