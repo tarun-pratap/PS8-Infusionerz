43024a238453c2284d6ee068566447f298c6ca79
Devrastogii280 <devrastogii280@gmail.com>
2023-07-21 15:29:27 +0530

    added-fields-designing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import Login from "./components/Login";
+import PolicyForm from "./components/PolicyForm";
 import PolicyPage from "./components/PolicyPage";
 import Sign from "./components/Sign";
 import { Route, Routes } from "react-router-dom";
@@ -10,6 +11,7 @@ function App() {
         <Route path="/new-user-register" element={<Sign />} />
         <Route path="/login-user" element={<Login />} />
         <Route path="/policy-redirect-page" element={<PolicyPage />} />
+        <Route path="/policy-form" element={<PolicyForm />} />
       </Routes>
     </>
   );
--- frontend/src/components/PolicyForm.jsx
+++ frontend/src/components/PolicyForm.jsx
@@ -1,10 +1,30 @@
-import { Input, InputGroup, InputLeftAddon, Switch } from '@chakra-ui/react'
-import React from 'react'
+import { Input, InputGroup, InputLeftAddon, InputLeftElement, InputRightAddon, Select, Switch, Textarea } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import male from '../images/form/male.png'
 import female from '../images/form/female.png'
 import trans from '../images/form/trans.png'
 
 const PolicyForm = () => {
+
+  // Values   
+  const [email, setEmail] = useState('')
+  const [pincode, setPincode] = useState('')
+  const [address, setAddress] = useState('')
+  const [income, setIncome] = useState('')
+  const [occupation, setOccupation] = useState('')
+  
+  // Switch Value
+  const [genderValue, setGenderClick] = useState('')
+  const [nriswitchValue, setNriSwitchValue] = useState('')  
+
+  const handleGenderSwitchChange = (value) => {
+    setGenderClick(value)
+  }
+
+  const handleNriSwitchChange = (value) => {
+    setNriSwitchValue(value)
+  }
+
   return (
     <>
        <section className='h-[auto] w-full bg-primary'>
@@ -26,13 +46,13 @@ const PolicyForm = () => {
                                     <label className='text-[1.6rem]'>Gender</label>
                                     <div className='flex gap-x-10 mt-[1.5rem]'>
                                         <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}> 
-                                            <Switch id='male' colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'><img src={male} alt="male" className='w-[3rem]' /></span>
+                                            <Switch id='male' isChecked={genderValue === 'male'} onChange={() => handleGenderSwitchChange('male')} colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'><img src={male} alt="male" className='w-[3rem]' /></span>
                                         </div>
                                         <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}> 
-                                            <Switch id='female' colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'><img src={female} alt="female" /></span>
+                                            <Switch id='female' isChecked={genderValue === 'female'} onChange={() => handleGenderSwitchChange('female')} colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'><img src={female} alt="female" /></span>
                                         </div>
                                         <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}> 
-                                            <Switch id='trans' colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'><img src={trans} alt="trans" /></span>
+                                            <Switch id='trans' isChecked={genderValue === 'trans'} onChange={() => handleGenderSwitchChange('trans')} colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'><img src={trans} alt="trans" /></span>
                                         </div>
                                     </div>
                                 </div> 
@@ -46,7 +66,97 @@ const PolicyForm = () => {
                                 <div className='mt-10 px-28 ml-10'>
                                     <label className='text-[1.6rem]'>Date of Birth</label> <br />
                                     <Input autoComplete='off' id='dob' marginTop={'1.5rem'} placeholder="Select Date of Birth..." size="md" type="date" width={'80%'} fontSize={'1.2rem'} height={'4rem'} />
-                                </div>                                                                                                                             
+                                </div>                                          
+                                </div>  
+                                <div><hr className='w-full h-[0.2rem] rounded-3xl bg-zinc-200 mt-16 opacity-80' /></div>                               
+                                <div className='text-[2.4rem] font-semibold text-[#011F3F] flex gap-x-5 mt-10 px-16'><div className='w-[4rem] h-[4rem] flex justify-center items-center border-2 rounded-full bg-[#0072BC] text-white border-[#0c5b90]'>2</div> <div>Second, fill some general details</div></div>      
+                                <div className='w-full h-auto mt-10'>
+                                <div className='px-28 py-10 ml-10'>
+                                    <label className='text-[1.6rem]'>Email Address</label> <br />
+                                    <InputGroup marginTop={'1.5rem'}>
+                                        <InputLeftElement pointerEvents='none' children='@' fontSize={'1.5rem'} marginTop={'0.8rem'}></InputLeftElement>
+                                        <Input autoComplete='off' id='email' value={email} onChange={(e) => setEmail(e.target.value)} type='email' width={'80%'} fontSize={'1.2rem'} height={'4rem'} placeholder='Enter your Email Address...'/>
+                                    </InputGroup>
+                                </div> 
+                                <div className='mt-1 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Pincode</label> <br />
+                                    <InputGroup marginTop={'1.5rem'}>
+                                        <InputLeftElement pointerEvents='none'><i class="bi bi-geo-alt-fill text-[1.4rem] mt-5"></i></InputLeftElement>
+                                        <Input value={pincode} onChange={(e) => setPincode(e.target.value)} autoComplete='off' id='pincode' type='number' width={'80%'} fontSize={'1.2rem'} height={'4rem'} placeholder='Enter your Pincode...' />
+                                    </InputGroup> 
+                                </div>                         
+                                <div className='mt-10 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Address</label> <br />
+                                    <Textarea autoComplete='off' id='address' placeholder='Enter your Address...' marginTop={'1.5rem'} fontSize={'1.2rem'} width={'80%'} height={'8rem'} value={address} onChange={(e) => setAddress(e.target.value)} />
+                                </div>
+                                <div className='mt-10 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Education</label> <br />
+                                    <Select id='education' placeholder='Select your Education...' marginTop={'1.5rem'} width={'80%'} fontSize={'1.2rem'} height={'4rem'}>
+                                        <option value='option1' fontSize={'1.2rem'}>Higher Secondary</option>
+                                        <option value='option2' fontSize={'1.2rem'}>Under Graduate</option>
+                                        <option value='option3' fontSize={'1.2rem'}>Phd</option>
+                                        <option value='option4' fontSize={'1.2rem'}>Post Graduate</option>
+                                    </Select>
+                                </div>           
+                                <div className='mt-10 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Annual Income</label> <br />
+                                    <InputGroup marginTop={'1.5rem'}>
+                                        <Input autoComplete='off' id='income' type='number' width={'73%'} fontSize={'1.2rem'} height={'4rem'} placeholder='Enter your Annual Income...' />
+                                        <InputRightAddon height={'4rem'} fontSize={'1.2rem'} children='(in lakhs)' />
+                                    </InputGroup> 
+                                </div>               
+                                <div className='mt-10 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Occupation</label> <br />
+                                    <Input autoComplete='off' id='occupation' type='text' width={'80%'} fontSize={'1.2rem'} height={'4rem'}  marginTop={'1.5rem'} placeholder='Enter your Occupation...' />
+                                </div>
+                                </div>
+                                <div><hr className='w-full h-[0.2rem] rounded-3xl bg-zinc-200 mt-16 opacity-80' /></div>       
+                                <div className='text-[2.4rem] font-semibold text-[#011F3F] flex gap-x-5 mt-16 px-16'><div className='w-[4rem] h-[4rem] flex justify-center items-center border-2 rounded-full bg-[#0072BC] text-white border-[#0c5b90]'>3</div> <div>Third, some policy specific questions</div></div>
+                                <div className='w-full h-auto mt-10'>
+                                <div className='px-28 py-10 ml-10'>
+                                    <label className='text-[1.6rem]'>Are you a NRI ?</label>
+                                    <div className='flex gap-x-5 mt-3'>
+                                        <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                           
+                                        <Switch id='nri_yes' isChecked={nriswitchValue === 'yes'} onChange={() => handleNriSwitchChange('yes')} colorScheme='green' size={'lg'} /> <span className='ml-3 -mt-1'>Yes</span>
+                                        </div>
+                                        <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                       
+                                        <Switch id='nri_no' isChecked={nriswitchValue === 'no'} onChange={() => handleNriSwitchChange('no')} colorScheme='red' size={'lg'} /> <span className='ml-3 -mt-1'>No</span>                                  
+                                        </div>
+                                    </div>
+                                </div>  
+                                <div className='mt-1 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Are you a Smoker ?</label>
+                                    <div className='flex gap-x-5 mt-3'>
+                                    <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                           
+                                        <Switch id='smoke_yes' colorScheme='green' size={'lg'} isChecked={smokerswitchValue === 'yes'} onChange={() => handleSmokerSwitchChange('yes')} /> <span className='ml-3 -mt-1'>Yes</span>
+                                        </div>
+                                        <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                       
+                                        <Switch id='smoke_no' colorScheme='red' size={'lg'} isChecked={smokerswitchValue === 'no'} onChange={() => handleSmokerSwitchChange('no')} /> <span className='ml-3 -mt-1'>No</span>                                  
+                                        </div>
+                                    </div>
+                                </div>   
+                                <div className='mt-10 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Are you an existing Bajaj Employee ?</label>
+                                    <div className='flex gap-x-5 mt-3'>
+                                    <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                           
+                                        <Switch id='em_yes' colorScheme='green' size={'lg'} isChecked={emswitchValue === 'yes'} onChange={() => handleEmSwitchChange('yes')} /> <span className='ml-3 -mt-1'>Yes</span>
+                                        </div>
+                                        <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                       
+                                        <Switch id='em_no' colorScheme='red' size={'lg'} isChecked={emswitchValue === 'no'} onChange={() => handleEmSwitchChange('no')} /> <span className='ml-3 -mt-1'>No</span>                                  
+                                        </div>
+                                    </div>
+                                </div>  
+                                <div className='mt-10 px-28 ml-10'>
+                                    <label className='text-[1.6rem]'>Are you having Diabetes ?</label>
+                                    <div className='flex gap-x-5 mt-3'>
+                                    <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                           
+                                        <Switch id='diab_yes' colorScheme='green' size={'lg'} isChecked={diabeteswitchValue === 'yes'} onChange={() => handleDiabetesSwitchChange('yes')} /> <span className='ml-3 -mt-1'>Yes</span>
+                                        </div>
+                                        <div className={`w-[8rem] h-[4rem] flex justify-center items-center text-[1.6rem]`}>                                       
+                                        <Switch id='diab_no' colorScheme='red' size={'lg'} isChecked={diabeteswitchValue === 'no'} onChange={() => handleDiabetesSwitchChange('no')} /> <span className='ml-3 -mt-1'>No</span>                                  
+                                        </div>
+                                    </div>
+                                </div>                                    
                             </div>
                         </div>
                         </form> 
