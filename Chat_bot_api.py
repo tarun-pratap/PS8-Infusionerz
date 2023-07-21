96bee3450219889cd3e2c87056d3452a6a0d25b8
Hardik Dharmani <ddharmani99@gmail.com>
2023-07-21 14:36:14 +0530

    Create retrievers for all 5 policy products stored in policy_retrievers dictionary.

--- Chat_bot_api.py
+++ Chat_bot_api.py
@@ -5,6 +5,8 @@ import openai
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 
+from langchain.vectorstores import Chroma
+
 sys.path.append('../..')
 
 from dotenv import load_dotenv, find_dotenv
@@ -15,3 +17,22 @@ openai.api_key  = os.environ['OPENAI_API_KEY']
 # Open AI Embeddings
 embedding = OpenAIEmbeddings()
 chatllm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+# Define the directories for policy embeddings
+policy_embeddings_directories = {
+    "Goal Assure": "./pickled policy embeddings/Goal Assure",
+    "ACE": "./pickled policy embeddings/ACE",
+    "Diabetic Care": "./pickled policy embeddings/Diabetic Care",
+    "E Touch": "./pickled policy embeddings/e-touch",
+    "Assured Wealth": "./pickled policy embeddings/Assured Wealth"
+}
+
+# Initialize the policy retrievers
+policy_retrievers = {}
+for policy_name, embeddings_directory in policy_embeddings_directories.items():
+    policy_db = Chroma(
+        persist_directory=embeddings_directory,
+        embedding_function=embedding
+    )
+    policy_retriever = policy_db.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+    policy_retrievers[policy_name] = policy_retriever
